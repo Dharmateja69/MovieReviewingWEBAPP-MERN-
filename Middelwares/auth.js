5aba5c8629c5fda94703e899f3cd6cb8c58be416
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-30 19:37:57 +0530

    Sigin done-45 running

---
+++ Middelwares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../Models/Users");
+const { senderror } = require("../utils/helper")
+
+
+exports.isAuth = async (req, res, next) => {
+    const token = req.headers?.authorization;
+    if (!token) return senderror(res, "Invalid token!", 401);
+    const jwtToken = token.split("Bearer ")[1];
+    if (!jwtToken) return senderror(res, "Invalid token!", 401);
+    // console.log(jwtToken);
+    const decode = jwt.verify(jwtToken, process.env.JWT_SECRET);
+    const { userId } = decode
+
+    const user = await User.findById(userId)
+    // console.log(JSON.stringify(user))
+    if (!user) return senderror(res, "Invalid token user not found!", 404)
+    // res.json({ user: { id: user._id, name: user.name, email: user.email } })
+    req.user = user
+    next()
+
+}
--- Routes/User.js
+++ Routes/User.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const { isAuth } = require("../Middelwares/auth")
+const { isValidPassResetToken } = require("../Middelwares/user");
+
+
+
 const {
     createcon,
     VerifyEmail,
@@ -6,22 +11,42 @@ const {
     forgetPassword,
     resetPassword,
     sendResetPasswordTokenStatus,
-    signin
+    signin,
 } = require("../Controller/ConUsers");
-const { UserValidator, validate, validatorPassword, Siginvalidator } = require("../Middelwares/validator");
-const { isValidPassResetToken } = require("../Middelwares/user");
+const {
+    UserValidator,
+    validate,
+    validatorPassword,
+    Siginvalidator,
+} = require("../Middelwares/validator");
+
 
 const Router = express.Router();
 
 Router.post("/create", UserValidator, validate, createcon);
 Router.post("/verify-email", VerifyEmail); // 19/10
 Router.post("/resend-email-verfication-token", resendEmailVerficationToken);
+
 // 11-12-2024
 Router.post("/forget-password", forgetPassword);
 //11-12-2024(6:30)
-Router.post("/verify-pass-reset-token", isValidPassResetToken, sendResetPasswordTokenStatus);
+Router.post(
+    "/verify-pass-reset-token",
+    isValidPassResetToken,
+    sendResetPasswordTokenStatus
+);
 //11-12-2024(7:13)
-Router.post("/reset-password", validatorPassword, validate, isValidPassResetToken, resetPassword);
+Router.post(
+    "/reset-password",
+    validatorPassword,
+    validate,
+    isValidPassResetToken,
+    resetPassword
+);
 //12-12-24
 Router.post("/signin", Siginvalidator, validate, signin);
+Router.get("/is-auth", isAuth, (req, res) => {
+    const { user } = req;
+    res.json({ user: { id: user._id, name: user.name, email: user.email } })
+})
 module.exports = Router;
